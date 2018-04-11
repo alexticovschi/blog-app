e692a269dbc58728313941071837eec501d20e35
AlexTicovschi <alexmticovschi@gmail.com>
2018-04-11 21:57:04 +0100

    handling errors locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ import axios from 'axios';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -28,6 +29,10 @@ class Blog extends Component {
 
                 this.setState({ posts: updatedPosts });
             })
+            .catch(error => {
+                //console.log(error);
+                this.setState({ error: true })
+            })
     }
 
     postSelectedHandler = (id) => {
@@ -35,13 +40,19 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}/>
-        })
+        let posts = <p style={{textAlign:'center'}}>Something went wrong!</p>   
+        // if there's no error, display the post
+        if(!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post 
+                        key={post.id} 
+                        title={post.title} 
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
+                )
+            })
+        }
 
         return (
             <div>
