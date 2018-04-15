c71a992ecd815ea630d49d45f63b24f2f6a4cc0a
AlexTicovschi <alexmticovschi@gmail.com>
2018-04-15 10:39:25 +0100

    passing route parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from '../../containers/Blog/FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -26,6 +27,7 @@ class Blog extends Component {
                 </header>
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
