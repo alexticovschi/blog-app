b1b2d242531559056c7fc87daf4dfb3520ba9e03
AlexTicovschi <alexmticovschi@gmail.com>
2018-04-15 09:16:49 +0100

    styling the active routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 // import axios from 'axios';
 
 import './Blog.css';
@@ -13,13 +13,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/new-post">New Post</Link></li>
+                            <li><NavLink 
+                                to="/" 
+                                exact 
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to="/new-post">New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
