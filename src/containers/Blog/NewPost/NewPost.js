e88a7a023b201afbd85aa039187ac08e21dedd7e
AlexTicovschi <alexmticovschi@gmail.com>
2018-04-17 14:56:31 +0100

    conditional redirects

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 import './NewPost.css';
@@ -7,7 +8,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'John Rambo'
+        author: 'John Rambo',
+        submitted: null
     }
 
     componentDidMount() {
@@ -24,12 +26,14 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response.data)
+                this.setState({submitted: true});
             })
     }
 
     render () {
         return (
             <div className="NewPost">
+                {this.state.submitted && <Redirect to="/posts" />}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
